39e1655530f23157845a019489d7afe86c382c5b
Doe Agudey Daniel <agudeydaniel8@gmail.com>
2024-10-19 22:04:51 +0000

    Update 367_validPerfectSquare.py

--- Leetcode/easy/367_validPerfectSquare.py
+++ Leetcode/easy/367_validPerfectSquare.py
@@ -1,8 +1,7 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        res = num ** 0.5 
+        res = num ** 0.5
 
-        if res % 1 == 0:
-            return True
-        else:
-            return False
+        return res % 1 == 0
+
+      
