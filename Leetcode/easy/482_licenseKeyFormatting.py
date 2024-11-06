b18bae63107052a4a293986045e62aa21be31541
Doe Agudey Daniel <agudeydaniel8@gmail.com>
2024-11-06 20:08:41 +0000

    Update 482_licenseKeyFormatting.py

--- Leetcode/easy/482_licenseKeyFormatting.py
+++ Leetcode/easy/482_licenseKeyFormatting.py
@@ -1,16 +1,10 @@
 class Solution:
     def licenseKeyFormatting(self, s: str, k: int) -> str:
-        res = ''
+        res = s.replace('-','')
         result = ''
         count = 0
         mul = 0
 
-        for i in range(len(s)):
-            if s[i].isalnum():
-                if s[i].isalpha():
-                    res += s[i].upper()
-                else:
-                    res += s[i]
         res = res[::-1]
 
         while count < len(res):
@@ -20,7 +14,7 @@ class Solution:
                 count += k
                 mul += k
 
-        return result[::-1]
+        return result[::-1].upper()
 
 
         
