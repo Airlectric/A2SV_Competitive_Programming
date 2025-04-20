c3b8be4676135a469df49b7fae29a4eb29b8bc1f
Doe Agudey Daniel <agudeydaniel8@gmail.com>
2025-04-21 07:44:33 +0800

    [5] [Time Beats: 52.68%] [Memory Beats: 61.51%] - LeetPush

---
+++ Leetcode/medium/Longest-Palindromic-Substring.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        substr = ''
+        lenSub = 0
+
+        for i in range(len(s)):
+            pt1, pt2 = i, i
+            while pt1 >= 0 and pt2 < len(s) and s[pt1] == s[pt2]:
+                if (pt2 - pt1 + 1) > lenSub:
+                    substr = s[pt1 : pt2+1]
+                    lenSub = (pt2 - pt1 + 1)
+                pt1-=1
+                pt2+=1
+
+            pt1, pt2 = i, i+1
+            while pt1 >= 0 and pt2 < len(s) and s[pt1] == s[pt2]:
+                if (pt2 - pt1 + 1) > lenSub:
+                    substr = s[pt1 : pt2+1]
+                    lenSub = (pt2 - pt1 + 1)
+                pt1 -= 1
+                pt2 += 1
+
+        return substr
+
+        
